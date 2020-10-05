2ffcb90c65bbabe69ebb256a00d8b072444f6b3e
Shashank N <shashank.n@vvce.ac.in>
2020-10-05 12:40:24 +0530

    C++ program to Print ASCII Value.

---
+++ Program-7.cpp
@@ -0,0 +1,12 @@
+//Design and develop a C++ program to Print ASCII Value.
+
+//The objective of the program is to display the ASCII value of the character.
+
+//Instructions
+
+//Read a character from the standard input.
+//Display the ASCII value of the entered character using standard output
+
+//Expected input and output
+//Input: Enter a character  p
+//Output: The ASCII value of entered character p is 112
