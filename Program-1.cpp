ed3b75d3f813fc8e47f7fb4c031eeac0eadcb8f7
Shashank N <shashank.n@vvce.ac.in>
2020-10-05 10:44:05 +0530

    Update the missing statements in the given code

---
+++ Program-1.cpp
@@ -0,0 +1,23 @@
+//Complete the following missing statements in the below sample code which performs addition of two numbers.
+// 1)Initially, read two numbers from the standard input
+// 2)Perform the addition operation.
+// 3)Finally, display the result on the screen.
+
+//Expected input & output:
+//Enter two integers: 2 3
+//The sum of two numbers is 5
+
+#include<iostream>
+void main()
+{
+    int x, y, s;    
+    cout << "Enter two numbers: ";
+    cin >> x>>y;
+
+    // sum of two numbers in stored in variable s    
+  //COMPLETE THE CODE TO PERFORM ADDITION OF TWO NUMBERS
+
+    // Prints the result
+//WRITE THE STATEMENT WHICH PRINTS THE RESULT AND DISPLAY THE SAME
+    
+}
