96102517732d96ed7f264cfdc56e2eb4212e03a3
Shashank N <shashank.n@vvce.ac.in>
2020-10-05 11:30:01 +0530

    C++ program to multiply two integer numbers.

---
+++ Program-4.cpp
@@ -0,0 +1,12 @@
+//Develop a C++ program to multiply two integer numbers.
+
+//Instructions
+
+//In this program, the user enters two integer number of his choice with the help of standard input.
+//The multiplication operation is performed.
+//The Output for this program is the product of two entered numbers that will be displayed on the standard output.
+
+
+//Expected input and output
+//Input: Enter two numbers: 10 2
+//Output: The product of two numbers is 20
