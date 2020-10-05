04f5b8894f24caa3836bfa71665f240f1ed24382
Shashank N <shashank.n@vvce.ac.in>
2020-10-05 12:44:42 +0530

    C++ program to Find Size of a Variable

---
+++ Program-8.cpp
@@ -0,0 +1,11 @@
+//Design and develop a C++ program to Find Size of a Variable
+
+//Instructions
+
+//The user has to print the size of the variable for different primitive data types using sizeof operator in the standard output
+
+//Expected Output
+//Size of char: 1 byte
+//Size of int: 4 bytes
+//Size of float: 4 bytes
+//Size of double: 8 bytes
