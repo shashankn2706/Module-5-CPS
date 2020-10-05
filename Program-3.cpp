dfe6d995e8b5909d4dc68abed7dcdf6cf756833b
Shashank N <shashank.n@vvce.ac.in>
2020-10-05 11:27:25 +0530

    C++ Program to display the number entered by the user.

---
+++ Program-3.cpp
@@ -0,0 +1,9 @@
+//Develop a C++ program to display the number entered by the user.
+
+//Instructions
+//In this program, the user enters a number of his choice with the help of standard input.
+//The Output for this program is the number entered by the user will be displayed on the standard output.
+
+//Expected input and output
+//Input: Enter the number  8
+//Output: The entered number is 8
