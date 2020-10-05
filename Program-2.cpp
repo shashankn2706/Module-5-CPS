f959508e82f1386c7c03f19d78ba51c6c9f29d9f
Shashank N <shashank.n@vvce.ac.in>
2020-10-05 11:24:45 +0530

    Update the missing statements in the given code

---
+++ Program-2.cpp
@@ -0,0 +1,24 @@
+//In the given code snippet to calculate the area of rectangle, some essential statements are missing. Complete the following C++ code which computes the area of a rectangle.
+
+//Initially to calculate the area of rectangle we require length and breadth. Read the values with the help of standard input.
+//Perform the operation to compute the area of rectangle.
+//Finally, display the result on the standard output.
+
+
+//Expected input & output:
+//Enter the length of rectangle 4
+//Enter the breadth of rectangle 8
+//Area of rectangle 32
+
+#include <iostream>
+void main() {
+ int ; //DECLARE SUITABLE VARIABLES
+ cout << "\nEnter the Length of Rectangle : ";
+ cin>>;  //complete the statement
+ cout << "\nEnter the Breadth of Rectangle : ";
+ cin>>; //complete the statement
+
+   //Fill the suitable executable statement which computes area of rectangle
+
+   cout << "\nArea of Rectangle: " << ; //complete the statement
+}
