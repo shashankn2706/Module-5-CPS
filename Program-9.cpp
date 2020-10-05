e6a767662c68f6ca386533c158d7aae99e976709
Shashank N <shashank.n@vvce.ac.in>
2020-10-05 12:49:07 +0530

    C++ program to calculate the area of triangle

---
+++ Program-9.cpp
@@ -0,0 +1,9 @@
+//Design and develop a C++ program to claculate the area of triangle
+
+//In this program, the user enters the value of base and height with the help of standard input. 
+//The Output for this program is the area of the triangle that will be displayed on the standard output.
+//The formula to find area of triangle is 0.5*b*h
+
+//Expected input and output
+//Input: Enter the value of base and height: 10 2
+//Output: The area of triangle is: 10
