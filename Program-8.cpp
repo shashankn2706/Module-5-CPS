91b01fd6c2172c675096c3d2c0e0f2737ffa33d9
Shashank N <shashank.n@vvce.ac.in>
2020-12-19 11:41:36 +0530

    Update Program-8.cpp

--- Program-8.cpp
+++ Program-8.cpp
@@ -1,11 +1,8 @@
-//Design and develop a C++ program to Find Size of a Variable
-
+//Design and develop a C++ program to check whether a given number is odd or even
 //Instructions
 
 //The user has to print the size of the variable for different primitive data types using sizeof operator in the standard output
 
-//Expected Output
-//Size of char: 1 byte
-//Size of int: 4 bytes
-//Size of float: 4 bytes
-//Size of double: 8 bytes
+//Expected Input and Output
+//Enter the number: 5
+//5 is odd number
