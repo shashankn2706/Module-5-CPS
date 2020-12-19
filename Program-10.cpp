98870ebada1cf15bbd309f5023682183d7769958
Shashank N <shashank.n@vvce.ac.in>
2020-12-19 12:19:52 +0530

    C++ Program to Check Whether a character is Vowel or Consonant

---
+++ Program-10.cpp
@@ -0,0 +1,11 @@
+//C++ Program to Check Whether a character is Vowel or Consonant
+
+//Five alphabets a, e, i, o and u are known as vowels. All other alphabets except these 5 alphabets are known are consonants.
+//This program assumes that the user will always enter an alphabet.
+
+//Write a simple C++ program to Check Whether a character is Vowel or Consonant
+
+//Expected input and output
+
+//Enter an alphabet:a
+//a is an vowel
