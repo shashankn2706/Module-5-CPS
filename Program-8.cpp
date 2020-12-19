e799b8375b804c3714c137906a53456a5b540193
Shashank N <shashank.n@vvce.ac.in>
2020-12-19 12:09:50 +0530

    C++ Program to check number is odd or even

---
+++ Program-8.cpp
@@ -0,0 +1,12 @@
+//C++ Program to check a given whether a given number is odd or even
+
+//Integers which are perfectly divisible by 2 are called even numbers and those integers which are not perfectly divisible by 2 are not known as odd number.
+
+//Instructions
+//Write a program using C++ to check a given whether a given number is odd or even
+//Enter any number from the standard input
+//check the given number is odd or even
+
+//Expected input and output
+//Enter a given number- 3
+//3 is an odd number
