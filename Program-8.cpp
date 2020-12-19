395aa6f08125681b95cebbddc53c4a6a36fc7cbf
Shashank N <shashank.n@vvce.ac.in>
2020-12-19 11:57:55 +0530

    Delete Program-8.cpp

--- Program-8.cpp
+++
@@ -1,8 +0,0 @@
-//Design and develop a C++ program to check whether a given number is odd or even
-//Instructions
-
-//The user has to check the given number entered by a user is an odd or even number
-
-//Expected Input and Output
-//Enter the number: 5
-//5 is odd number
