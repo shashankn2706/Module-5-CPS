cba10e3a4bf06535cc07e104764c224b7df3e468
Shashank N <shashank.n@vvce.ac.in>
2020-12-21 12:13:40 +0530

    C++ program to calculate quotient and remainder.

---
+++ Program-6.cpp
@@ -0,0 +1,9 @@
+//Develop a C++ program to calculate quotient and remainder.
+
+//Instructions
+//In this program, the user enters a number of his choice with the help of standard input.
+//The Output for this program is the quotient and remainder.
+
+//Expected input and output
+//Input: Enter two numbers 10 2
+//Output: The quotient and remainder is 5 and 0
